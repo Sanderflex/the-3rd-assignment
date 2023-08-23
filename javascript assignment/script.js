9725a609caeeea22bbd0b3b2e319e2d75bafb6f2
Sanderflex <142084197+Sanderflex@users.noreply.github.com>
2023-08-23 14:24:00 +0100

    Add files via upload

---
+++ javascript assignment/script.js
@@ -0,0 +1,42 @@
+alert('GOOD DAY TO YOU SIR')
+
+var s1 = document.getElementById("s1");
+    s1.style.backgroundColor = "red";
+       s1.style.color = "white";
+        s1.style.padding = "10px";
+        s1.style.border ="solid";s1.style.borderColor ="black";s1.style.margin ="10px"
+var s2 = document.getElementById("s2");
+    s2.style.backgroundColor = "orange";
+    s2.style.color = "white";
+    s2.style.padding = "10px";
+    s2.style.border ="solid";s2.style.borderColor ="black";s2.style.margin ="10px"
+var s3 = document.getElementById("s3");
+    s3.style.backgroundColor = "yellow";
+    s3.style.color = "white";
+    s3.style.padding = "10px";
+    s3.style.border ="solid";s3.style.borderColor ="black";s3.style.margin ="10px"
+var s4 = document.getElementById("s4");
+    s4.style.backgroundColor = "green";
+    s4.style.color = "white";
+    s4.style.padding = "10px";
+    s4.style.border ="solid";s4.style.borderColor ="black";s4.style.margin ="10px"
+var s5 = document.getElementById("s5");
+    s5.style.backgroundColor = "blue";
+    s5.style.color = "white";
+    s5.style.padding = "10px";
+    s5.style.border ="solid";s5.style.borderColor ="black";s5.style.margin ="10px"
+    var s6 = document.getElementById("s6");
+    s6.style.backgroundColor = "purple";
+    s6.style.color = "white";
+    s6.style.padding = "10px";
+    s6.style.border ="solid";s6.style.borderColor ="black";s6.style.margin ="10px"
+    var s7 = document.getElementById("s7");
+    s7.style.backgroundColor = "violet";
+    s7.style.color = "white";
+    s7.style.padding = "10px";
+    s7.style.border ="solid";s7.style.borderColor ="black";s7.style.margin ="10px"
+    var s8 = document.getElementById("s8");
+    s8.style.backgroundColor = "gold";
+    s8.style.color = "white";
+    s8.style.padding = "10px";
+    s8.style.border ="solid";s8.style.borderColor ="black";s8.style.margin ="10px";s8.pd
